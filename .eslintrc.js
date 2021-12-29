600ab0eb245cab9ad6ee1c268b56c10f04f47ee7
Flavio Brassesco <brassescoflavio@gmail.com>
2021-12-29 20:50:48 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+};
